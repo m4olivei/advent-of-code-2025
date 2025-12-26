abb2067d8dd79abb05a05cb0beb8d1bee41459fb
Matthew Oliveira <m4olivei@gmail.com>
2025-12-26 16:29:19 -0500

    day 3 pt 2

--- advent-of-code-3.py
+++ advent-of-code-3.py
@@ -1,14 +1,24 @@
+def max_joltage_digit(battery_bank, digits_remaining, from_index):
+    end_index = -digits_remaining + 1
+    subsection = battery_bank[from_index:] if end_index == 0 else battery_bank[from_index:end_index]
+    return max(subsection)
+
 # Main execution block
 if __name__ == "__main__":
     joltage = 0
+    joltage_digits = 12
     with open('./assets/input-3.txt', 'r') as f:
         for line in f:
             # Parse our input line.
             line = line.strip("\n")
-            batteries = list(map(int ,line))
-            firstBattery = max(batteries[:-1])
-            firstBatteryIndex = batteries.index(firstBattery)
-            secondBattery = max(batteries[firstBatteryIndex + 1:])
-            bankJoltage = int(str(firstBattery) + str(secondBattery))
-            joltage += bankJoltage
+            batteries = list(map(int, line))
+            bank_joltage = []
+            from_index = 0
+            for i in range(joltage_digits):
+                max_digit = max_joltage_digit(batteries, joltage_digits - i, from_index)
+                from_index = batteries.index(max_digit, from_index) + 1
+                bank_joltage.append(max_digit)
+            joltage += int(''.join(str(n) for n in bank_joltage))
+
     print('Total joltage: ', joltage)
+
